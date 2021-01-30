4bdca82bf4042800f573b5664093cb474f5f80cc
unknown <amanda.mellerin@gmail.com>
2021-01-30 13:19:14 +0100

    adding axios lib to use API News

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    nameList: []
+  }
+// make the GET request to fetch data from the URL then using promise function to handle response.
+  componentDidMount() {
+    axios.get('http://newsapi.org/v2/top-headlines?' +
+    'country=us&' +
+    'apiKey=7a18809ef30f4836a8db7babf64587e8')
+      .then(res => {
+        const nameList = res.data.articles;
+        this.setState({ nameList });
+      })
+  }
+
+  render() {
+   const {nameList} = this.state;
+   return (
+     <View style={styles.container}>
+        <Text>Bonjour</Text>
+        <Text>
+          { 
+          nameList.map(article => <Text>{article.title}</Text>)
+          }
+        </Text>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
---
+++ Readme.txt
@@ -0,0 +1,6 @@
+Pas mal de galères au début pour installer le projet avec create-react-app car mes versions de node et npm étaient pas à jour
+Pas l'habitude de dev sous windows mais sous ubuntu donc temps de prise en main (je déteste le powershell :c )
+P'tit pb pour accéder à l'app via l'appli Expo sur android mais réglé avec l'installation d'adb pour une connexion en mode tunnel 
+
+Maintenant que l'app est installée et poussée sur github, je passe à la connexion à l'API 
+ install de Reactotron pour debug
